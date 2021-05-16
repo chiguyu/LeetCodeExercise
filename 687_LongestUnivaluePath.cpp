45f3000a07ef4142ea60a97bbc02b5f172bcdd50
Hao <haoxiaowei308@sina.com>
2021-05-16 23:51:25 +0800

    Recactor 687_LongestUnivaluePath
    Signed-off-by: Hao <haoxiaowei308@sina.com>

--- 687_LongestUnivaluePath.cpp
+++ 687_LongestUnivaluePath.cpp
@@ -9,28 +9,33 @@ struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 }; 
 
 class Solution {
 public:
-    // CalculateLength By DFS
+    /*
+       Description  : CalculateLength By DFS
+       Return Value : Max length of left-sub-tree & right-sub-tree
+       Parameters   : 1) cur : current node  2) lengthMax reference
+    */
     int CalculateLength(TreeNode* cur, int& lengthMax) {
         if (cur == nullptr) {
             return 0;
         }
 
         int lengthLeft = 0;
-        int lengthRight = 0;
-        int lengthOfLeft = CalculateLength(cur->left, lengthMax);
-        int lengthOfRigtht = CalculateLength(cur->right, lengthMax);
-
+        int lengthOfLeftSubTree = CalculateLength(cur->left, lengthMax);
         if (cur->left != nullptr && cur->val == cur->left->val) {
-            lengthLeft = lengthOfLeft + 1;
+            lengthLeft = lengthOfLeftSubTree + 1;
         }
 
+        int lengthRight = 0;
+        int lengthOfRigthtSubTree = CalculateLength(cur->right, lengthMax);
         if (cur->right != nullptr && cur->val == cur->right->val) {
-            lengthRight = lengthOfRigtht + 1;
+            lengthRight = lengthOfRigthtSubTree + 1;
         }
 
         lengthMax = max(lengthMax, lengthLeft + lengthRight);
@@ -39,7 +44,9 @@ public:
 
     int longestUnivaluePath(TreeNode* root) {
         int lengthMax = 0;
-        CalculateLength(root, lengthMax);
+        if (root != nullptr) {
+            CalculateLength(root, lengthMax);
+        }
         return lengthMax;
     }
 };
